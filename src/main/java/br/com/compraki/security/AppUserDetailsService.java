8df9bbd0a3ae59a13ef3e4bf5d27590720bca6ea
Manoel Albino Neto <manoelanagr@gmail.com>
2017-05-04 11:57:14 -0300

    Adição coluna nome em usuario; tinha comitado o atributo confirmaSenha no local; oque estava impossibilitando o login no sistema

--- src/main/java/br/com/compraki/security/AppUserDetailsService.java
+++ src/main/java/br/com/compraki/security/AppUserDetailsService.java
@@ -16,31 +16,32 @@ import org.springframework.stereotype.Service;
 
 import br.com.compraki.model.Usuario;
 import br.com.compraki.repository.Usuarios;
+import br.com.compraki.security.UsuarioSistema;
 
 @Service
 public class AppUserDetailsService implements UserDetailsService {
 
-	@Autowired
-	private Usuarios usuarios;
+    @Autowired
+    private Usuarios usuarios;
 
-	@Override
-	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-		Optional<Usuario> usuarioOptional = usuarios.findByEmail(email);
-		Usuario usuario = usuarioOptional
-				.orElseThrow(() -> new UsernameNotFoundException("Usuario e/ou senha incorretos"));
-		// return new User(usuario.getEmail(), usuario.getSenha(),
-		// getPermissoes(usuario));
-		return new UsuarioSistema(usuario, getPermissoes(usuario));
-	}
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<Usuario> usuarioOptional = usuarios.findByEmail(email);
+        Usuario usuario = usuarioOptional
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario e/ou senha incorretos"));
+        // return new User(usuario.getEmail(), usuario.getSenha(),
+        // getPermissoes(usuario));
+        return new UsuarioSistema(usuario, getPermissoes(usuario));
+    }
 
-	private Collection<? extends GrantedAuthority> getPermissoes(Usuario usuario) {
-		Set<SimpleGrantedAuthority> authorities = new HashSet<SimpleGrantedAuthority>(0);
+    private Collection<? extends GrantedAuthority> getPermissoes(Usuario usuario) {
+        Set<SimpleGrantedAuthority> authorities = new HashSet<SimpleGrantedAuthority>(0);
 
-		List<String> permissoes = usuarios.permissoes(usuario);
+        List<String> permissoes = usuarios.permissoes(usuario);
 
-		permissoes.forEach(p -> authorities.add(new SimpleGrantedAuthority(p.toUpperCase())));
+        permissoes.forEach(p -> authorities.add(new SimpleGrantedAuthority(p.toUpperCase())));
 
-		return authorities;
-	}
+        return authorities;
+    }
 
 }
