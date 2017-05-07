83bf3b7fe27852c32e7cb77cbd66feb5afddf737
manoel-an <manoel-an@pc-manoel>
2017-05-07 14:45:27 -0300

    Chamada css em layoutPadrao

---
+++ src/main/resources/static/javascripts/cadastro-usuario.js
@@ -0,0 +1,19 @@
+var Compraki = Compraki || {};
+
+Compraki.CadastroUsuario = (function() {
+	
+	function CadastroUsuario() {
+
+	}
+	
+	CadastroUsuario.prototype.iniciar = function(event) {
+
+	}
+	
+	return CadastroUsuario;
+}());
+
+$(function() {
+	var cadastroUsuario = new Compraki.CadastroUsuario();
+	cadastroUsuario.iniciar();
+});
