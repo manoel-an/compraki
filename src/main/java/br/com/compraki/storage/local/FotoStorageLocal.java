f7e9a6a58920beff6b0e1f8248392537553c4b9e
manoel-an <manoel-an@pc-manoel>
2017-05-23 19:44:49 -0300

    variavel path de upload

--- src/main/java/br/com/compraki/storage/local/FotoStorageLocal.java
+++ src/main/java/br/com/compraki/storage/local/FotoStorageLocal.java
@@ -14,65 +14,65 @@ import br.com.compraki.storage.FotoStorage;
 
 public class FotoStorageLocal implements FotoStorage {
 
-    private static String system;
+	private static String system;
 
-    private Path local;
+	private Path local;
 
-    public FotoStorageLocal() {
-        this(getDefault().getPath(
-                getSystem().contains("Windows") ? System.getenv("USERPROFILE") : System.getenv("HOME"), ".photouser"));
-    }
+	public FotoStorageLocal() {
+		this(getDefault().getPath(
+				getSystem().contains("Windows") ? System.getenv("USERPROFILE") : System.getenv("HOME"), ".photouser"));
+	}
 
-    public FotoStorageLocal(Path path) {
-        this.local = path;
-        criarPastas();
-    }
+	public FotoStorageLocal(Path path) {
+		this.local = path;
+		criarPastas();
+	}
 
-    @Override
-    public String salvarFoto(MultipartFile[] files) {
-        String novoNome = null;
-        if (files != null && files.length > 0) {
-            MultipartFile arquivo = files[0];
-            novoNome = renomearArquivo(arquivo.getOriginalFilename());
-            try {
-                arquivo.transferTo(
-                        new File(this.local.toAbsolutePath().toString() + getDefault().getSeparator() + novoNome));
-            } catch (IOException e) {
-                throw new RuntimeException("Erro salvando a foto na pasta temporária", e);
-            }
-        }
+	@Override
+	public String salvarFoto(MultipartFile[] files) {
+		String novoNome = null;
+		if (files != null && files.length > 0) {
+			MultipartFile arquivo = files[0];
+			novoNome = renomearArquivo(arquivo.getOriginalFilename());
+			try {
+				String path = this.local.toAbsolutePath().toString() + getDefault().getSeparator() + novoNome;
+				arquivo.transferTo(new File(path));
+			} catch (IOException e) {
+				throw new RuntimeException("Erro salvando a foto na pasta temporária", e);
+			}
+		}
 
-        return novoNome;
-    }
+		return novoNome;
+	}
 
-    @Override
-    public byte[] recuperarFoto(String nome) {
-        try {
-            return Files.readAllBytes(this.local.resolve(nome));
-        } catch (IOException e) {
-            throw new RuntimeException("Erro lendo a foto", e);
-        }
-    }
+	@Override
+	public byte[] recuperarFoto(String nome) {
+		try {
+			return Files.readAllBytes(this.local.resolve(nome));
+		} catch (IOException e) {
+			throw new RuntimeException("Erro lendo a foto", e);
+		}
+	}
 
-    private void criarPastas() {
-        try {
-            Files.createDirectories(this.local);
-            this.local = getDefault().getPath(this.local.toString());
-            Files.createDirectories(this.local);
-        } catch (IOException e) {
-            throw new RuntimeException("Erro criando pasta para salvar foto", e);
-        }
-    }
+	private void criarPastas() {
+		try {
+			Files.createDirectories(this.local);
+			this.local = getDefault().getPath(this.local.toString());
+			Files.createDirectories(this.local);
+		} catch (IOException e) {
+			throw new RuntimeException("Erro criando pasta para salvar foto", e);
+		}
+	}
 
-    private String renomearArquivo(String nomeOriginal) {
-        String novoNome = UUID.randomUUID().toString() + "_" + nomeOriginal;
-        return novoNome;
-    }
+	private String renomearArquivo(String nomeOriginal) {
+		String novoNome = UUID.randomUUID().toString() + "_" + nomeOriginal;
+		return novoNome;
+	}
 
-    public static String getSystem() {
-        if (system == null) {
-            system = System.getProperty("os.name");
-        }
-        return system;
-    }
+	public static String getSystem() {
+		if (system == null) {
+			system = System.getProperty("os.name");
+		}
+		return system;
+	}
 }
