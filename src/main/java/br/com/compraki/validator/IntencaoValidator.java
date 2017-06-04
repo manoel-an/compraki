0e41bc21914f4738901a229cb1a78d51d0d8ec07
manoel-an <manoel-an@pc-manoel>
2017-06-03 23:27:45 -0300

    Validação para marca e modelo em cadastro de IT

--- src/main/java/br/com/compraki/validator/IntencaoValidator.java
+++ src/main/java/br/com/compraki/validator/IntencaoValidator.java
@@ -10,18 +10,19 @@ import br.com.compraki.model.IntencaoCompra;
 @Component
 public class IntencaoValidator implements Validator {
 
-    @Override
-    public boolean supports(Class<?> clazz) {
-        return IntencaoCompra.class.equals(clazz);
-    }
+	@Override
+	public boolean supports(Class<?> clazz) {
+		return IntencaoCompra.class.equals(clazz);
+	}
 
-    @Override
-    public void validate(Object target, Errors errors) {
-        ValidationUtils.rejectIfEmpty(errors, "modelo.codigo", "", "É necessário selecionar um modelo");
-        // ValidationUtils.rejectIfEmpty(errors, "acessorios.codigo", "", "É
-        // necessario selecionar pelo menos um acessório");
-        ValidationUtils.rejectIfEmpty(errors, "cores", "", "É necessário selecionar pelo menos uma cor");
-        ValidationUtils.rejectIfEmpty(errors, "acessorios", "", "É necessário selecionar pelo menos um acessório");
-    }
+	@Override
+	public void validate(Object target, Errors errors) {
+		ValidationUtils.rejectIfEmpty(errors, "modelo.fabricante", "", "Ops! Escolha a marca do veículo desejado");
+		ValidationUtils.rejectIfEmpty(errors, "modelo", "", "Ops! Escolha o modelo do veículo desejado");
+		// ValidationUtils.rejectIfEmpty(errors, "acessorios.codigo", "", "É
+		// necessario selecionar pelo menos um acessório");
+		ValidationUtils.rejectIfEmpty(errors, "cores", "", "É necessário selecionar pelo menos uma cor");
+		ValidationUtils.rejectIfEmpty(errors, "acessorios", "", "É necessário selecionar pelo menos um acessório");
+	}
 
 }
