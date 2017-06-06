9006b78bad8700588e32a5e1fa0589a74e83fb26
Manoel Albino Neto <manoelanagr@gmail.com>
2017-06-06 13:49:57 -0300

    Retirada comentário. Para não conflitar com relacionamento de IT

--- src/main/java/br/com/compraki/model/Interacao.java
+++ src/main/java/br/com/compraki/model/Interacao.java
@@ -1,46 +1,24 @@
 package br.com.compraki.model;
 
 import java.io.Serializable;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 
-import javax.persistence.Column;
-import javax.persistence.Embedded;
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
-import javax.persistence.Transient;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
-import org.hibernate.validator.constraints.Email;
-import org.hibernate.validator.constraints.NotBlank;
-
-import br.com.compraki.enuns.EnumSexo;
-import br.com.compraki.enuns.TipoPessoa;
-import br.com.compraki.model.helper.PessoaHelper;
-import br.com.compraki.validation.AtributoConfirmacao;
-
-//@Entity
-//@Table(name = "interacao")
+@Entity
+@Table(name = "interacao")
 public class Interacao implements Serializable {
 
-	private static final long serialVersionUID = -8354269525264410932L;
+    private static final long serialVersionUID = -8354269525264410932L;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long codigo;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long codigo;
 
-	//@Enumerated(EnumType.STRING)
-	//private EnumSexo sexo;
-
-	
-	
+    // @Enumerated(EnumType.STRING)
+    // private EnumSexo sexo;
 
 }
