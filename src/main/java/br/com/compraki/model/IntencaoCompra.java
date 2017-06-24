a2a5d3b766454a752eec13bd46a30794707b47cf
Manoel Albino Neto <manoelanagr@gmail.com>
2017-06-24 14:09:50 -0300

    Alteração relacionamentos

--- src/main/java/br/com/compraki/model/IntencaoCompra.java
+++ src/main/java/br/com/compraki/model/IntencaoCompra.java
@@ -17,7 +17,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
@@ -38,131 +37,128 @@ import br.com.compraki.model.veiculo.ModeloVeiculo;
 @Table(name = "intencao_de_compra")
 public class IntencaoCompra {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long codigo;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long codigo;
 
-    @NotBlank(message = "Ops! Escolha o ano do veículo desejado")
-    private String ano;
+	@NotBlank(message = "Ops! Escolha o ano do veículo desejado")
+	private String ano;
 
-    @NotNull(message = "Ops! Valor é obrigatório")
-    private BigDecimal valor;
+	@NotNull(message = "Ops! Valor é obrigatório")
+	private BigDecimal valor;
 
-    // Escolher entre por exemplo alcool, gasolina, flex
-    @NotBlank(message = "Ops! Escolha qual combustível para o veículo desejado")
-    @Column(name = "tipo_combustivel")
-    private String tipoCombustivel;
+	// Escolher entre por exemplo alcool, gasolina, flex
+	@NotBlank(message = "Ops! Escolha qual combustível para o veículo desejado")
+	@Column(name = "tipo_combustivel")
+	private String tipoCombustivel;
 
-    @NotBlank(message = "Ops! As características são importantes para uma intenção de compra")
-    @Column(name = "outras_caracteristicas")
-    private String outrasCaracteristicas;
+	@NotBlank(message = "Ops! As características são importantes para uma intenção de compra")
+	@Column(name = "outras_caracteristicas")
+	private String outrasCaracteristicas;
 
-    @NotBlank(message = "Ops! Potência do veículo também ajuda")
-    private String potencia;
+	@NotBlank(message = "Ops! Potência do veículo também ajuda")
+	private String potencia;
 
-    @NotBlank(message = "Ops! A descrição é muito importante")
-    private String descricao;
+	@NotBlank(message = "Ops! A descrição é muito importante")
+	private String descricao;
 
-    @NotBlank(message = "Ops! Ajuda muito se você colocar se sua roda é comum ou uma mais específica. Se não sabe coloque qualquer")
-    @Column(name = "tipo_de_roda")
-    private String tipoDeRoda;
+	@NotBlank(message = "Ops! Ajuda muito se você colocar se sua roda é comum ou uma mais específica. Se não sabe coloque qualquer")
+	@Column(name = "tipo_de_roda")
+	private String tipoDeRoda;
 
-    @Column(name = "aro_roda")
-    private String aroRoda;
+	@Column(name = "aro_roda")
+	private String aroRoda;
 
-    @NotBlank(message = "Ops! É importante colocar o Estado de preferência")
-    @Column(name = "uf_preferencia")
-    private String uf;
+	@NotBlank(message = "Ops! É importante colocar o Estado de preferência")
+	@Column(name = "uf_preferencia")
+	private String uf;
 
-    @Column(name = "cidade_preferencia")
-    private String cidadePreferencia;
+	@Column(name = "cidade_preferencia")
+	private String cidadePreferencia;
 
-    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
-    @JoinColumn(name = "codigo_modelo")
-    private ModeloVeiculo modelo;
+	@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+	@JoinColumn(name = "codigo_modelo")
+	private ModeloVeiculo modelo;
 
-    // @NotBlank(message="Ops! Esqueceu o tipo de veculo")
-    @Column(name = "tipo_de_veiculo")
-    @Enumerated(EnumType.STRING)
-    private TipoVeiculo tipoVeiculo;
+	// @NotBlank(message="Ops! Esqueceu o tipo de veculo")
+	@Column(name = "tipo_de_veiculo")
+	@Enumerated(EnumType.STRING)
+	private TipoVeiculo tipoVeiculo;
 
-    @Column(name = "status_de_intencao")
-    @Enumerated(EnumType.STRING)
-    private StatusIntencao statusIntencao;
+	@Column(name = "status_de_intencao")
+	@Enumerated(EnumType.STRING)
+	private StatusIntencao statusIntencao;
 
-    @OneToOne(fetch = FetchType.LAZY)
-    private Interacao proposta;
+	@Transient
+	private IntencaoHelper intencaoHelper;
 
-    @Transient
-    private IntencaoHelper intencaoHelper;
+	@ManyToOne
+	@JoinColumn(name = "codigo_usuario")
+	private Usuario usuario;
 
-    @ManyToOne
-    @JoinColumn(name = "codigo_usuario")
-    private Usuario usuario;
+	@ManyToMany(fetch = FetchType.LAZY)
+	@JoinTable(name = "intencao_acessorios_desejados", joinColumns = @JoinColumn(name = "codigo_intencao"), inverseJoinColumns = @JoinColumn(name = "codigo_acessorio"))
+	private List<Acessorio> acessorios;
 
-    @ManyToMany(fetch = FetchType.LAZY)
-    @JoinTable(name = "intencao_acessorios_desejados", joinColumns = @JoinColumn(name = "codigo_intencao") , inverseJoinColumns = @JoinColumn(name = "codigo_acessorio") )
-    private List<Acessorio> acessorios;
+	@ManyToMany(fetch = FetchType.LAZY)
+	@JoinTable(name = "intencao_cores_desejadas", joinColumns = @JoinColumn(name = "codigo_intencao"), inverseJoinColumns = @JoinColumn(name = "codigo_cor"))
+	private List<Cor> cores;
 
-    @ManyToMany(fetch = FetchType.LAZY)
-    @JoinTable(name = "intencao_cores_desejadas", joinColumns = @JoinColumn(name = "codigo_intencao") , inverseJoinColumns = @JoinColumn(name = "codigo_cor") )
-    private List<Cor> cores;
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "data_criacao")
+	private Date dataCriacao;
 
-    @Temporal(TemporalType.TIMESTAMP)
-    @Column(name = "data_criacao")
-    private Date dataCriacao;
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "data_modificacao")
+	private Date dataModificacao;
 
-    @Temporal(TemporalType.TIMESTAMP)
-    @Column(name = "data_modificacao")
-    private Date dataModificacao;
+	@Transient
+	private String cidade;
 
-    @Transient
-    private String cidade;
+	@PrePersist
+	@PreUpdate
+	public void configuraDatasCriacaoAlteracao() {
+		this.dataModificacao = new Date();
 
-    @PrePersist
-    @PreUpdate
-    public void configuraDatasCriacaoAlteracao() {
-        this.dataModificacao = new Date();
+		if (this.dataCriacao == null) {
+			this.dataCriacao = new Date();
+		}
+	}
 
-        if (this.dataCriacao == null) {
-            this.dataCriacao = new Date();
-        }
-    }
+	// Getters and setters
+	public Long getCodigo() {
+		return codigo;
+	}
 
-    // Getters and setters
-    public Long getCodigo() {
-        return codigo;
-    }
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
 
-    public void setCodigo(Long codigo) {
-        this.codigo = codigo;
-    }
+	public String getAno() {
+		return ano;
+	}
 
-    public String getAno() {
-        return ano;
-    }
+	public void setAno(String ano) {
+		this.ano = ano;
+	}
 
-    public void setAno(String ano) {
-        this.ano = ano;
-    }
+	public BigDecimal getValor() {
+		return valor;
+	}
 
-    public BigDecimal getValor() {
-        return valor;
-    }
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
 
-    public void setValor(BigDecimal valor) {
-        this.valor = valor;
-    }
+	public String getTipoCombustivel() {
+		return tipoCombustivel;
+	}
 
-    public String getTipoCombustivel() {
-        return tipoCombustivel;
-    }
+	public void setTipoCombustivel(String tipoCombustivel) {
+		this.tipoCombustivel = tipoCombustivel;
+	}
 
-    public void setTipoCombustivel(String tipoCombustivel) {
-        this.tipoCombustivel = tipoCombustivel;
-    }
-
-    public String getOutrasCaracteristicas() {
+	public String getOutrasCaracteristicas() {
 		return outrasCaracteristicas;
 	}
 
@@ -171,170 +167,162 @@ public class IntencaoCompra {
 	}
 
 	public String getPotencia() {
-        return potencia;
-    }
+		return potencia;
+	}
 
-    public void setPotencia(String potencia) {
-        this.potencia = potencia;
-    }
+	public void setPotencia(String potencia) {
+		this.potencia = potencia;
+	}
 
-    public String getDescricao() {
-        return descricao;
-    }
+	public String getDescricao() {
+		return descricao;
+	}
 
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
 
-    public String getTipoDeRoda() {
-        return tipoDeRoda;
-    }
+	public String getTipoDeRoda() {
+		return tipoDeRoda;
+	}
 
-    public void setTipoDeRoda(String tipoDeRoda) {
-        this.tipoDeRoda = tipoDeRoda;
-    }
+	public void setTipoDeRoda(String tipoDeRoda) {
+		this.tipoDeRoda = tipoDeRoda;
+	}
 
-    public String getAroRoda() {
-        return aroRoda;
-    }
+	public String getAroRoda() {
+		return aroRoda;
+	}
 
-    public void setAroRoda(String aroRoda) {
-        this.aroRoda = aroRoda;
-    }
+	public void setAroRoda(String aroRoda) {
+		this.aroRoda = aroRoda;
+	}
 
-    public String getCidadePreferencia() {
-        return cidadePreferencia;
-    }
+	public String getCidadePreferencia() {
+		return cidadePreferencia;
+	}
 
-    public void setCidadePreferencia(String cidadePreferencia) {
-        this.cidadePreferencia = cidadePreferencia;
-    }
+	public void setCidadePreferencia(String cidadePreferencia) {
+		this.cidadePreferencia = cidadePreferencia;
+	}
 
-    public ModeloVeiculo getModelo() {
-        return modelo;
-    }
+	public ModeloVeiculo getModelo() {
+		return modelo;
+	}
 
-    public void setModelo(ModeloVeiculo modelo) {
-        this.modelo = modelo;
-    }
+	public void setModelo(ModeloVeiculo modelo) {
+		this.modelo = modelo;
+	}
 
-    public TipoVeiculo getTipoVeiculo() {
-        if (tipoVeiculo == null) {
-            tipoVeiculo = TipoVeiculo.CARRO;
-        }
-        return tipoVeiculo;
-    }
+	public TipoVeiculo getTipoVeiculo() {
+		if (tipoVeiculo == null) {
+			tipoVeiculo = TipoVeiculo.CARRO;
+		}
+		return tipoVeiculo;
+	}
 
-    public void setTipoVeiculo(TipoVeiculo tipoVeiculo) {
-        this.tipoVeiculo = tipoVeiculo;
-    }
+	public void setTipoVeiculo(TipoVeiculo tipoVeiculo) {
+		this.tipoVeiculo = tipoVeiculo;
+	}
 
-    public StatusIntencao getStatusIntencao() {
-        return statusIntencao;
-    }
+	public StatusIntencao getStatusIntencao() {
+		return statusIntencao;
+	}
 
-    public void setStatusIntencao(StatusIntencao statusIntencao) {
-        this.statusIntencao = statusIntencao;
-    }
+	public void setStatusIntencao(StatusIntencao statusIntencao) {
+		this.statusIntencao = statusIntencao;
+	}
 
-    public Interacao getProposta() {
-        return proposta;
-    }
+	public IntencaoHelper getIntencaoHelper() {
+		if (intencaoHelper == null) {
+			intencaoHelper = new IntencaoHelper();
+		}
+		return intencaoHelper;
+	}
 
-    public void setProposta(Interacao proposta) {
-        this.proposta = proposta;
-    }
+	public void setIntencaoHelper(IntencaoHelper intencaoHelper) {
+		this.intencaoHelper = intencaoHelper;
+	}
 
-    public IntencaoHelper getIntencaoHelper() {
-        if (intencaoHelper == null) {
-            intencaoHelper = new IntencaoHelper();
-        }
-        return intencaoHelper;
-    }
+	public Usuario getUsuario() {
+		return usuario;
+	}
 
-    public void setIntencaoHelper(IntencaoHelper intencaoHelper) {
-        this.intencaoHelper = intencaoHelper;
-    }
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
 
-    public Usuario getUsuario() {
-        return usuario;
-    }
+	public List<Acessorio> getAcessorios() {
+		return acessorios;
+	}
 
-    public void setUsuario(Usuario usuario) {
-        this.usuario = usuario;
-    }
+	public void setAcessorios(List<Acessorio> acessorios) {
+		this.acessorios = acessorios;
+	}
 
-    public List<Acessorio> getAcessorios() {
-        return acessorios;
-    }
+	public Date getDataCriacao() {
+		return dataCriacao;
+	}
 
-    public void setAcessorios(List<Acessorio> acessorios) {
-        this.acessorios = acessorios;
-    }
+	public void setDataCriacao(Date dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
 
-    public Date getDataCriacao() {
-        return dataCriacao;
-    }
+	public Date getDataModificacao() {
+		return dataModificacao;
+	}
 
-    public void setDataCriacao(Date dataCriacao) {
-        this.dataCriacao = dataCriacao;
-    }
+	public void setDataModificacao(Date dataModificacao) {
+		this.dataModificacao = dataModificacao;
+	}
 
-    public Date getDataModificacao() {
-        return dataModificacao;
-    }
+	public String getCidade() {
+		return cidade;
+	}
 
-    public void setDataModificacao(Date dataModificacao) {
-        this.dataModificacao = dataModificacao;
-    }
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
 
-    public String getCidade() {
-        return cidade;
-    }
+	public String getUf() {
+		return uf;
+	}
 
-    public void setCidade(String cidade) {
-        this.cidade = cidade;
-    }
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
 
-    public String getUf() {
-        return uf;
-    }
+	public List<Cor> getCores() {
+		return cores;
+	}
 
-    public void setUf(String uf) {
-        this.uf = uf;
-    }
+	public void setCores(List<Cor> cores) {
+		this.cores = cores;
+	}
 
-    public List<Cor> getCores() {
-        return cores;
-    }
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		return result;
+	}
 
-    public void setCores(List<Cor> cores) {
-        this.cores = cores;
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        IntencaoCompra other = (IntencaoCompra) obj;
-        if (codigo == null) {
-            if (other.codigo != null)
-                return false;
-        } else if (!codigo.equals(other.codigo))
-            return false;
-        return true;
-    }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		IntencaoCompra other = (IntencaoCompra) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		return true;
+	}
 
 }
