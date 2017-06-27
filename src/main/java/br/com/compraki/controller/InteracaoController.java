bf86df3f435b918b29f95f600aa27f918a43160a
claudio <cmartinsgyn@gmail.com>
2017-06-27 01:27:21 -0300

    Lista Propostas Recebidas - Esboço interação comprador

--- src/main/java/br/com/compraki/controller/InteracaoController.java
+++ src/main/java/br/com/compraki/controller/InteracaoController.java
@@ -1,55 +1,67 @@
 package br.com.compraki.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import br.com.compraki.enuns.TipoVeiculo;
-import br.com.compraki.model.IntencaoCompra;
 import br.com.compraki.model.Interacao;
-import br.com.compraki.model.Usuario;
-import br.com.compraki.model.veiculo.Carro;
 import br.com.compraki.repository.Carros;
+import br.com.compraki.repository.Interacoes;
+import br.com.compraki.repository.filter.IntencaoFilter;
 import br.com.compraki.security.UsuarioSistema;
 
 @Controller
 @RequestMapping("/interacao")
 public class InteracaoController {
 
-	private static final String ITR_VIEW = "decidir";
+	private static final String PROPOSTA_ENVIADA_VIEW = "interacao/fornecedor/PesquisaPropostasEnviadas";
 	
 	@Autowired
 	private Carros carros;
 	
-	@GetMapping("/{codigo}")
+	@Autowired
+	private Interacoes interacoes;
+	
+	/*@GetMapping("/{codigo}")
 	public ModelAndView editar(@PathVariable("codigo") IntencaoCompra intencaoCompra,
 			@AuthenticationPrincipal User user, Interacao propostaFonecedor) {
 		UsuarioSistema usuarioSistema = (UsuarioSistema) user;
 		ModelAndView mv = getDefaultObjectsModelAndViewDeProposta(usuarioSistema.getUsuario(), intencaoCompra,
 				propostaFonecedor);
 		return mv;
-	}
+	}*/
+	
+	@GetMapping("/propostasEnviadas")
+    public ModelAndView pesquisar(@AuthenticationPrincipal User user, IntencaoFilter intencaoFilter,
+            BindingResult result, @PageableDefault(size = 7) Pageable pageable, HttpServletRequest httpServletRequest) {
+        UsuarioSistema usuarioSistema = (UsuarioSistema) user;
+        List<Interacao> listaPropostasEnviadas = interacoes.findAll();
+        ModelAndView mv = new ModelAndView(PROPOSTA_ENVIADA_VIEW);
+        /*PageWrapper<Interacao> paginaWrapper = new PageWrapper<>(this.intencaoService.getIntencaoCompras()
+                .filtrar(usuarioSistema.getUsuario(), intencaoFilter, pageable), httpServletRequest);
+        mv.addObject("fabricantes", this.fabricantes.findAll());
+        mv.addObject("tipoVeiculos", TipoVeiculo.values());
+        mv.addObject("cores", this.cores.findAll());
+        mv.addObject("acessorios", this.acessorios.findAll());
+        mv.addObject("tiposCombustivel", TipoCombustivel.values());
+        mv.addObject("ufs", UF.values());
+        mv.addObject("potencias", PotenciaVeiculo.values());
+        mv.addObject("pagina", paginaWrapper);*/
+        mv.addObject("todasPropostasEnviadas", listaPropostasEnviadas);
+        
+        return mv;
+    }
 
-	// renderiza na view os objetos da Porposta do fornecedor
-	private ModelAndView getDefaultObjectsModelAndViewDeProposta(Usuario usuario, IntencaoCompra intencaoCompra,
-			Interacao propostaFornecedor) {
-		ModelAndView modelAndView = new ModelAndView(ITR_VIEW);
-
-		modelAndView.addObject("veiculos", carros.findByUsuarioAndFetchEager(usuario));
-
-		propostaFornecedor.setIntencaoCompra(intencaoCompra);
-		modelAndView.addObject("tipos", TipoVeiculo.values());
-		Carro carro = new Carro();
-		carro.setUsuario(usuario);
-		modelAndView.addObject("carro", carro);
-		modelAndView.addObject("cadastroVeiculo", Boolean.FALSE);
-		modelAndView.addObject("propostaFonecedor", propostaFornecedor);
-		return modelAndView;
-	}
-
+	
 }// fim
