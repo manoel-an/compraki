ee264515cb05666e1909520460b1d47ce7580bd5
claudio <cmartinsgyn@gmail.com>
2017-07-01 01:38:33 -0300

    Mais interacao...

--- src/main/java/br/com/compraki/controller/InteracaoController.java
+++ src/main/java/br/com/compraki/controller/InteracaoController.java
@@ -17,21 +17,29 @@ import org.springframework.web.servlet.ModelAndView;
 
 import br.com.compraki.model.Interacao;
 import br.com.compraki.repository.Carros;
+import br.com.compraki.repository.Fabricantes;
 import br.com.compraki.repository.Interacoes;
+import br.com.compraki.repository.ModelosVeiculos;
 import br.com.compraki.repository.filter.IntencaoFilter;
-import br.com.compraki.security.UsuarioSistema;
 
 @Controller
 @RequestMapping("/interacao")
 public class InteracaoController {
 
-	private static final String PROPOSTA_ENVIADA_VIEW = "interacao/fornecedor/PesquisaPropostasEnviadas";
+	private static final String PROPOSTA_ENVIADA_VIEW = "interacao/PesquisaPropostas";
 	
-	@Autowired
-	private Carros carros;
 	
 	@Autowired
 	private Interacoes interacoes;
+	
+	@Autowired
+    private Fabricantes fabricantes;
+	
+	@Autowired
+    private Carros veiculos;
+	
+	@Autowired
+    private ModelosVeiculos modelos;
 	
 	/*@GetMapping("/{codigo}")
 	public ModelAndView editar(@PathVariable("codigo") IntencaoCompra intencaoCompra,
@@ -42,12 +50,15 @@ public class InteracaoController {
 		return mv;
 	}*/
 	
-	@GetMapping("/propostasEnviadas")
+	@GetMapping("/propostas")
     public ModelAndView pesquisar(@AuthenticationPrincipal User user, IntencaoFilter intencaoFilter,
             BindingResult result, @PageableDefault(size = 7) Pageable pageable, HttpServletRequest httpServletRequest) {
-        UsuarioSistema usuarioSistema = (UsuarioSistema) user;
+        //UsuarioSistema usuarioSistema = (UsuarioSistema) user;
         List<Interacao> listaPropostasEnviadas = interacoes.findAll();
         ModelAndView mv = new ModelAndView(PROPOSTA_ENVIADA_VIEW);
+        mv.addObject("fabricantes", this.fabricantes.findAll());
+        mv.addObject("veiculos", this.veiculos.findAll());
+        mv.addObject("modelos", modelos.findAll());
         /*PageWrapper<Interacao> paginaWrapper = new PageWrapper<>(this.intencaoService.getIntencaoCompras()
                 .filtrar(usuarioSistema.getUsuario(), intencaoFilter, pageable), httpServletRequest);
         mv.addObject("fabricantes", this.fabricantes.findAll());
@@ -62,6 +73,6 @@ public class InteracaoController {
         
         return mv;
     }
-
 	
+		
 }// fim
